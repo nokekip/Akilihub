698fc56b4777aa23d04ff4e71ea6d5fe02044f1b
nokekip <kipnoke@gmail.com>
2024-02-09 15:12:44 +0300

    added event model

--- base/models.py
+++ base/models.py
@@ -56,3 +56,16 @@ class Message(models.Model):
     
     def __str__(self):
         return self.text
+    
+    
+class Event(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    name = models.CharField(max_length=200)
+    description = models.TextField()
+    date = models.DateTimeField()
+    link = models.TextField(null=True)
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
+    related_field = models.ForeignKey(Field, on_delete=models.CASCADE)
+    
+    def __str__(self):
+        return self.name
