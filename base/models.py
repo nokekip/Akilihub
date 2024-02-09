80e2bedc07df92de47f76682971184fc70fa10e9
nokekip <kipnoke@gmail.com>
2024-02-09 14:02:10 +0300

    added fiels model

--- base/models.py
+++ base/models.py
@@ -15,3 +15,11 @@ class User(AbstractUser):
     REQUIRED_FIELDS = []
     
 
+class Field(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    name = models.CharField(max_length=100)
+    desctiption = models.TextField()
+    
+    def __str__(self):
+        return self.name
+    
