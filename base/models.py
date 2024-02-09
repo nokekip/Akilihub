168697452e7c906e1721c25402cf45501a18f1c1
nokekip <kipnoke@gmail.com>
2024-02-09 13:29:57 +0300

    added custom user nodel

--- base/models.py
+++ base/models.py
@@ -1,3 +1,14 @@
+import uuid
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
+
+class User(AbstractUser):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    email = models.EmailField(unique=True, null=True)
+    name = models.CharField(max_length=100, null=True)
+    bio = models.TextField(null=True)
+    # avatar
+    
+    USERNAME_FIELD = 'email'
