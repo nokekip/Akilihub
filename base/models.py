57fa7f22a00af3a31b3663dfa5d2162a501e77a4
nokekip <kipnoke@gmail.com>
2024-02-09 15:06:28 +0300

    added message model for akili room chats

--- base/models.py
+++ base/models.py
@@ -46,3 +46,13 @@ class AkiliRoom(models.Model):
     
     def __str__(self):
         return self.name
+    
+class Message(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    room = models.ForeignKey(AkiliRoom, on_delete=models.CASCADE)
+    text = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    
+    def __str__(self):
+        return self.text
