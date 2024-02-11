6ea83dcb3ba75970a5a8fbf84391a27eeaee0ccc
nokekip <kipnoke@gmail.com>
2024-02-12 01:55:11 +0300

    route to register user

--- base/views.py
+++ base/views.py
@@ -1,3 +1,21 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .forms import CreateUserForm
 
 # Create your views here.
+
+# registering new user
+def register(request):
+    form = CreateUserForm()
+    if request.method == 'POST':
+        form = CreateUserForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            return redirect('home')
+    context = {'form': form}
+    return render(request, 'register.html', context)
+
+
+# home page
+def index(request):
+    return render(request, 'index.html')
+
