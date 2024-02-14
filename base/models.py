cfe7276d095b4b09f6d05d95e391d56519f17be5
nokekip <kipnoke@gmail.com>
2024-02-14 10:12:32 +0300

    Add ordering to AkiliRoom model

--- base/models.py
+++ base/models.py
@@ -44,6 +44,9 @@ class AkiliRoom(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
     
+    class Meta:
+        ordering = ['-updated_at', '-created_at']
+    
     def __str__(self):
         return self.name
     
